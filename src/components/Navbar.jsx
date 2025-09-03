97602db47e086b3d8c8b04bb9ce7d17a4dacd25c
bluemzzn <synthiawithsyren@gmail.com>
2025-09-04 00:50:14 +0700

    github test

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -4,7 +4,7 @@ function Navbar() {
 return (
     <div className='px-2.5 mt-4 mx-3 flex justify-between'>
         <div>
-            <h1 className='font-bold text-white'>Client</h1>
+            <h1 className='font-bold text-white'>Clients</h1>
         </div>
         <div>
             <input type='text' placeholder='Search...' className='border py-1 px-2 text-white rounded-md w-48 md:w-auto' />
